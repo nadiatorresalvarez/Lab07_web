0ab6b7a300d24bd7556397ef7f2311f88bdfbdc0
Nadia Torres <nadia.torres@tecsup.edu.pe>
2025-05-12 00:05:32 -0500

    page register

--- jwt-auth-react-app/src/pages/RegisterPage.js
+++ jwt-auth-react-app/src/pages/RegisterPage.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { register } from "../services/auth.service";
-import { Link } from "react-router-dom";
 import "../components/Register.css";
 
 const Register = () => {
